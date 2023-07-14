780327a9db89fd22e84f6f1cfaf2e7dafba64e03
William Moreira <william.moreira3@fatec.sp.gov.br>
2023-07-14 15:40:55 -0300

    Initial Commit

---
+++ StoneChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoneChallenge
+//
+//  Created by William on 14/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
